31268882843df8c9f3afdc17148afd152fa22dc4
Larissa Galan <jarbiedeleon123456789@example.com>
2026-09-18 16:41:24 +0800

    Disable refresh cookie for hosted auth

--- config/plugins.ts
+++ config/plugins.ts
@@ -28,7 +28,7 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin =>
     config: {
       jwtManagement: 'refresh',
       sessions: {
-        httpOnly: true,
+        httpOnly: false,
       },
     },
   },
